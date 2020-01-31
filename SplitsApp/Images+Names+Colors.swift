a984051daa66c46925a7f163ba9e545fcb00b5b2
leandroestrada1 <leandro.estrada@itsgroup.com.br>
2020-01-31 16:00:03 -0300

    Starting the splits app

---
+++ SplitsApp/Images+Names+Colors.swift
@@ -0,0 +1,24 @@
+//
+//  Images+Names+Colors.swift
+//  SplitsApp
+//
+//  Created by leandro de araujo estrada on 31/01/20.
+//  Copyright © 2020 Leandro Estrada. All rights reserved.
+//
+import Foundation
+import UIKit
+
+let camera = UIImage(named: "1")!
+let city = UIImage(named: "2")!
+let buffalo = UIImage(named: "3")!
+let bouqet = UIImage(named: "4")!
+let stand = UIImage(named: "5")!
+let urban = UIImage(named: "6")!
+
+let imageArray = [camera, city, buffalo, bouqet, stand, urban]
+
+//Names
+var namesArray = ["CAMERA","CITY","ANIMALS","FLOWERS","STANDS","URBAN"]
+
+//Colors
+let colorsArray = [#colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1), #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)]
